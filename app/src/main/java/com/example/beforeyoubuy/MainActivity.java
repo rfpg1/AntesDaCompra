36b99cbc087f83a21bc4e03cb05c1827e712ecc4
rfpg1 <frg8019@gmail.com>
2021-01-03 06:36:52 +0000

    Versão 3/01 6:36

--- app/src/main/java/com/example/beforeyoubuy/MainActivity.java
+++ app/src/main/java/com/example/beforeyoubuy/MainActivity.java
@@ -3,7 +3,14 @@ package com.example.beforeyoubuy;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.database.Cursor;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -15,6 +22,8 @@ import com.budiyev.android.codescanner.DecodeCallback;
 import com.budiyev.android.codescanner.ScanMode;
 import com.google.zxing.Result;
 
+import java.io.ByteArrayOutputStream;
+
 public class MainActivity extends AppCompatActivity {
 
     private CodeScanner mCodeScanner;
@@ -24,22 +33,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imageView = (ImageView) findViewById(R.id.imageView);
-        imageView.setVisibility(View.INVISIBLE);
-        DatabaseHandler dbh = new DatabaseHandler(this);
+        this.imageView = (ImageView) findViewById(R.id.imageView);
+        this.imageView.setVisibility(View.INVISIBLE);
         CodeScannerView scannerView = findViewById(R.id.scanner_view);
-        mCodeScanner = new CodeScanner(this, scannerView);
+        this.mCodeScanner = new CodeScanner(this, scannerView);
         //Caso queira dar scan a mais que um objeto continuamente
         //mCodeScanner.setScanMode(ScanMode.CONTINUOUS);
-        mCodeScanner.setDecodeCallback(new DecodeCallback() {
+        this.mCodeScanner.setDecodeCallback(new DecodeCallback() {
             @Override
             public void onDecoded(@NonNull Result result) {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
                         //TODO
-                        // fazer o result.getText() comparar com uma database
-                        imageView.setVisibility(View.VISIBLE);
+                        // Favoritos
+                        // Eventualmente ter mais exemplos
+                        // Criar um botão por baixo da imagem do produto que leva a uma outra imagem com mais detalhes
+                        // Botão canto superior esquerdo tipo definições
+
+                        if(result.getText().equals("google")){
+                            Toast.makeText(MainActivity.this, result.getText(), Toast.LENGTH_LONG).show();
+                            imageView.setVisibility(View.VISIBLE);
+                            imageView.setImageResource(R.drawable.araujo);
+                        } else if(result.getText().equals("firefox")){
+                            Toast.makeText(MainActivity.this, result.getText(), Toast.LENGTH_LONG).show();
+                            imageView.setVisibility(View.VISIBLE);
+                            imageView.setImageResource(R.drawable.dio);
+                        }
                     }
                 });
             }
@@ -51,6 +71,12 @@ public class MainActivity extends AppCompatActivity {
                 imageView.setVisibility(View.INVISIBLE);
             }
         });
+    }
+
+    private byte[] getBytes(Bitmap bitmap) {
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.PNG, 0, stream);
+        return stream.toByteArray();
     }
 
     @Override
