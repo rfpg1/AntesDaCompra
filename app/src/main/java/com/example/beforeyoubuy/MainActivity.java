97b741b845cf8afe13eec07e47ce5dfc5363343a
rfpg1 <frg8019@gmail.com>
2021-01-03 03:08:37 +0000

    ARAUJO

--- app/src/main/java/com/example/beforeyoubuy/MainActivity.java
+++ app/src/main/java/com/example/beforeyoubuy/MainActivity.java
@@ -5,22 +5,30 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.budiyev.android.codescanner.CodeScanner;
 import com.budiyev.android.codescanner.CodeScannerView;
 import com.budiyev.android.codescanner.DecodeCallback;
+import com.budiyev.android.codescanner.ScanMode;
 import com.google.zxing.Result;
 
 public class MainActivity extends AppCompatActivity {
 
     private CodeScanner mCodeScanner;
+    private Button button;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.button);
+        button.setVisibility(View.INVISIBLE);
         CodeScannerView scannerView = findViewById(R.id.scanner_view);
         mCodeScanner = new CodeScanner(this, scannerView);
+        //Caso queira dar scan a mais que um objeto continuamente
+        //mCodeScanner.setScanMode(ScanMode.CONTINUOUS);
         mCodeScanner.setDecodeCallback(new DecodeCallback() {
             @Override
             public void onDecoded(@NonNull Result result) {
@@ -29,6 +37,8 @@ public class MainActivity extends AppCompatActivity {
                     public void run() {
                         //TODO fazer o result.getText() comparar com uma database!
                         Toast.makeText(MainActivity.this, result.getText(), Toast.LENGTH_SHORT).show();
+                        button.setVisibility(View.VISIBLE);
+                        button.setText(result.getText());
                     }
                 });
             }
@@ -37,6 +47,7 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 mCodeScanner.startPreview();
+                button.setVisibility(View.INVISIBLE);
             }
         });
     }
